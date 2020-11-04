59603cae7ae457fe00df4ded47c05bf181e17c86
Shashank <saishashank85@gmail.com>
2020-11-04 12:25:34 +0000

    1

--- ras/y/modify_rasa_hf.py
+++ ras/y/modify_rasa_hf.py
@@ -1,21 +1,10 @@
 import rasa , os , shutil
-def copytree(src, dst, symlinks=False, ignore=None):
-        if not os.path.exists(dst):
-            os.makedirs(dst)
-        for item in os.listdir(src):
-            s = os.path.join(src, item)
-            d = os.path.join(dst, item)
-            if os.path.isdir(s):
-                copytree(s, d, symlinks, ignore)
-            else:
-                if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
-                    shutil.copy2(s, d)
+   
+rasapath = rasa.__file__[:-11] + "nlu/utils/hugging_face/"
 
-if __name__ == "__main__":
-    
-    rasapath = rasa.__file__[:-11] + "nlu/utils/hugging_face/"
-    
-    dir_path = os.path.dirname(os.path.realpath(__file__)) + "/hugging_face"
-    copytree(dir_path,rasapath)
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/hugging_face/"
+shutil.rmtree(rasapath)
+shutil.copytree(dir_path,rasapath)
+print("Done!")
 
 
